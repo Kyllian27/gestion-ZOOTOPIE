5c9cf20b93ff855909210c42c2bd6550a783026d
Bodex27 <zoubida13300@gmail.com>
2026-07-20 09:19:13 +0200

    feat : initialisation du projet

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-require_once "header.php"
+    require_once "header.php"
 ?>
-<div class="container">
-    <div class="card">
-        <div class="card-body">
-            <h1>Zoologik</h1>
-        </div>
-    </div>
-</div>
-<?php
-require_once "footer.php";
-?>
+    <html>
+    <link href="style.css" rel="stylesheet">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h1>Zoologik</h1>
+            </div>
 
+<?php
+    require_once "footer.php";
+?>
