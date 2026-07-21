c8472a79b9aee62c5b0647c1d27a6260cb8eef26
Bodex27 <zoubida13300@gmail.com>
2026-07-21 11:59:53 +0200

    Employe (#3)
    * feat : initialisation du projet
    
    * feat : initialisation du projet
    
    * feat : initialisation du projet

--- employe/result-update.php
+++ employe/result-update.php
@@ -5,8 +5,8 @@ require_once("../header.php");
     <div class="row">
         <div class="col">
             <?php
-            if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['poste'])) :
-                if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['poste'])) :
+            if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['poste']) && isset($_POST['id'])) :
+                if(empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['poste']) || empty($_POST['id'])) :
                     ?>
                     <div class="alert alert-danger">
                         Erreur, un champ est vide !
@@ -14,14 +14,15 @@ require_once("../header.php");
                 <?php
                 else:
                     try{
-                        $sql = "INSERT INTO employe(nom_emp, prenom_emp, poste_emp) VALUES(:nom, :prenom, :poste)";
+                        $sql = "UPDATE employe SET nom_emp = :nom, prenom_emp = :prenom, poste_emp = :poste WHERE id_employe = :id";
                         $stmt = $db->prepare($sql);
                         $stmt->bindValue(':nom', trim($_POST['nom']));
                         $stmt->bindValue(':prenom', trim($_POST['prenom']));
                         $stmt->bindValue(':poste', trim($_POST['poste']));
+                        $stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
                         $stmt->execute(); ?>
                         <div class="alert alert-success">
-                            L'employé a bien été créé.
+                            L'employé a bien été modifié.
                         </div>
                         <?php
                     } catch(PDOException $e){
@@ -35,7 +36,6 @@ require_once("../header.php");
                 </div>
             <?php
             endif;
-
             ?>
         </div>
     </div>
